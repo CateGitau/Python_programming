6d4aa5cd37dcbee46a979bd9b1b2b59a51361d59
cate-ammi <catherinegitau94@gmail.com>
2020-01-27 07:51:23 +0000

    changed time_conversion solution to the correct solution

--- Hackerrank/time_conversion.py
+++ Hackerrank/time_conversion.py
@@ -15,32 +15,24 @@ Note: Midnight is 12:00:00AM on a 12-hour clock, and 00:00:00 on a 24-hour
 clock. Noon is 12:00:00PM on a 12-hour clock, and 12:00:00 on a 24-hour clock. 
 """
 
-s = '07:05:45PM'
+s = '12:40:22AM'
 
 #print(time.split(':'))
 
 def timeConversion(s):
-    p = s.split(':')
-    time_day = p[len(p) - 1].lstrip('123456789')
-    vals = p[len(p) - 1][:len(time_day)]
+    #p = s.split(':')
+    time_day = s[-2:]
+    hr = s[0:2]
     
-    time = []
+    if time_day == 'PM' and s[0:2] != '12':
+        hr = str(int(s[0:2])+12)
+        
+    elif time_day == 'AM' and s[0:2] == '12':
+        hr = '00'
     
-    if time_day == 'PM':
-        time.append(str(int(p[0]) + 12))
-        time.append(p[1].zfill(2))
-        time.append(vals)
-        
-    else:
-        time.append(p[0])
-        time.append(p[1])
-        
-    return (':'.join(time))
-        
-        
-
-        
     
+    return hr + s[2:-2]
+        
     #print(''.join(hr, mm, sec))
     
 print(timeConversion(s))            
