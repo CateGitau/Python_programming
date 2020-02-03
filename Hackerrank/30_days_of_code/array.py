8ad7503e03e5d80465111484d5c275e3e5fe1138
AIMS <catherinegitau94@gmail.com>
2020-02-03 12:03:41 +0000

    added solution to reversing array

---
+++ Hackerrank/30_days_of_code/array.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  3 11:13:49 2020
+
+@author: aims
+"""
+
+arr = "1,2,3,4"
+
+print(' '.join(map(str, arr[::-1])))
--- Hackerrank/30_days_of_code/letsReview.py
+++ Hackerrank/30_days_of_code/letsReview.py
@@ -5,3 +5,20 @@ Created on Sat Feb  1 20:31:10 2020
 
 @author: aims
 """
+N = int(input())
+
+for i in range(0, N):
+
+    string = input()
+
+    for j in range(0, len(string)):
+        if j % 2 == 0:
+            print(string[j], end='')
+
+    print(" ", end='')
+
+    for j in range(0, len(string)):
+        if j % 2 != 0:
+            print(string[j], end='')
+
+    print("")
