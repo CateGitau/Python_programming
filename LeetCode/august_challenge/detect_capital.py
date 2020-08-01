ca8c9701142761c5b880fb7bd3390e7957a052c5
Catherine <catherinegitau94@gmail.com>
2020-08-01 16:09:04 +0000

    added solution to august challenge 1

---
+++ LeetCode/august_challenge/detect_capital.py
@@ -0,0 +1,47 @@
+'''
+Given a word, you need to judge whether the usage of capitals in it is right or not.
+
+We define the usage of capitals in a word to be right when one of the following cases holds:
+
+    All letters in this word are capitals, like "USA".
+    All letters in this word are not capitals, like "leetcode".
+    Only the first letter in this word is capital, like "Google".
+
+Otherwise, we define that this word doesn't use capitals in a right way. 
+'''
+
+
+a = "Lowercase"
+
+def detectCapital(a):
+    capital = 0
+    lower = 0
+    length = len(a)
+
+    if a.istitle():
+        return True
+    else:
+        for i in a:
+            if i.islower():
+                lower+= 1
+            else:
+                capital+=1
+        if capital == length or lower == length:
+            return True
+        else:
+            return False
+print(detectCapital(a))
+
+
+## oooorrrr:
+
+def easiersol(a):
+    if word.isupper():
+                return True
+            elif word[0].isupper() & word[1:].islower():
+                return True
+            elif word.islower():
+                return True
+            else:
+                return False
+
